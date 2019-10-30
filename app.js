aed28bae68e899f80dd23bdd6f3814b9caf94805
Vijayeta Rangarajan <vijayetar@gmail.com>
2019-10-29 20:16:31 -0700

    greeting alert

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ askName = prompt("What is your name?");
 //console.log('user Name', askName);
 
 //greet User
-var greetUser = "Hi there," + askName + "!";
+alert ("Hi there," + askName + "!");
+
 //console.log('greeting',greetUser);
-document.write(greetUser);
 
 //ready to play?
+function guessUser() {
 var userPlay = prompt("Are you ready to play the guessing game?");
+}
