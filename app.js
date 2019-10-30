a74133146d664080030535f0d06ba1993658dc33
Vijayeta Rangarajan <vijayetar@gmail.com>
2019-10-29 21:21:34 -0700

    added question3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict";
-/*
+
 //get User name
 var askName;
 askName = prompt("What is your name?");
@@ -8,7 +8,8 @@ askName = prompt("What is your name?");
 //greet User
 alert("Hi there, " + askName + "! Welcome to the Guessing Game!");
 //console.log('greeting',greetUser);
-*/
+
+
 
 //where was I born?
   var birthPlace = prompt("Was I born in India? Please answer yes/no");
@@ -20,4 +21,16 @@ alert("Hi there, " + askName + "! Welcome to the Guessing Game!");
     //console.log("You are wrong!!! I was born in India");
     alert("You are wrong!!! I was born in India");
   }
+
+
+//where did I do my medical training?
+var medTraining = prompt("Did I attend medical school in New Zealand? Please answer in yes/no");
+medTraining = medTraining.toLowerCase();
+if (medTraining === 'yes' || medTraining === 'y') {
+  //console.log("You are right!");
+  alert("You are right!")
+} else {
+  //console.log("You are wrong!!!");
+  alert("You are wrong!!! I attended medical school in New Zealand!");
+}
 
